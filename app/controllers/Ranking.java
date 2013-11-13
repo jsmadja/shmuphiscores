10ae2a9d70336be15c7049173b2b0748fb8de4cf
Julien Smadja <julien.smadja@gmail.com>
2013-11-13 09:01:36 +0100

    Optimisation creation image Ranking

--- app/controllers/Ranking.java
+++ app/controllers/Ranking.java
@@ -6,18 +6,16 @@ import plugins.RankingPicture;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
-import static com.google.common.io.Files.toByteArray;
 
 public class Ranking extends Controller {
 
     public static Result index(models.Game game) throws IOException {
-        BufferedImage bi = RankingPicture.createRankingPicture(game);
-        File dest = new File("/tmp/pouet");
-        ImageIO.write(bi, "PNG", dest);
-        byte[] bytes = toByteArray(dest);
+        BufferedImage image = RankingPicture.createRankingPicture(game);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        ImageIO.write(image, "PNG", stream);
+        byte[] bytes = stream.toByteArray();
         response().setContentType("image/png");
         return ok(bytes);
     }
