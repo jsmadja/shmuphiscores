37b5ff35dfc1429f1a3024c89307920b07be54e5
Julien Smadja <julien.smadja@gmail.com>
2013-11-13 09:05:45 +0100

    Renommage page vue Joueur

--- app/controllers/Player.java
+++ app/controllers/Player.java
@@ -6,7 +6,7 @@ import play.mvc.Result;
 public class Player extends Controller {
 
     public static Result index(models.Player player) {
-        return ok(views.html.player.render(player));
+        return ok(views.html.player_read.render(player));
     }
 
 }
