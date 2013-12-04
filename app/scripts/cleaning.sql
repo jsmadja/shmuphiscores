6e8005710c8f84f73b28ab57d890233170be5a33
Julien Smadja <julien.smadja@gmail.com>
2013-12-04 13:53:38 +0100

    ajout cleaning.sql

--- app/scripts/cleaning.sql
+++ app/scripts/cleaning.sql
@@ -118,4 +118,8 @@ UPDATE score
 SET player_id = (SELECT id FROM player WHERE name = 'Sulf\'')
 WHERE player_id IN (SELECT id FROM player WHERE name = 'Sulfateur');
 
+UPDATE score
+SET player_id = (SELECT id FROM player WHERE name = 'dodonpasim')
+WHERE player_id IN (SELECT id FROM player WHERE name = 'dodonpasism');
+
 DELETE FROM player WHERE id NOT IN (SELECT distinct player_id FROM score) AND shmup_user_id IS NULL
