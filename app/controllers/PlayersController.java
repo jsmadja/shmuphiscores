7b9e0e83168fec3755a564ebf5ebfd0e9a3ff844
Julien Smadja <julien.smadja@gmail.com>
2013-12-20 19:46:33 +0100

    Changements majeurs page des joueurs

--- app/controllers/PlayersController.java
+++ app/controllers/PlayersController.java
@@ -1,10 +1,14 @@
 package controllers;
 
 import com.avaje.ebean.Ebean;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 import models.Player;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -20,6 +24,12 @@ public class PlayersController extends Controller {
                 return player.name.compareToIgnoreCase(player2.name);
             }
         });
+        players = new ArrayList<Player>(Collections2.filter(players, new Predicate<Player>() {
+            @Override
+            public boolean apply(@Nullable Player player) {
+                return !player.scores.isEmpty();
+            }
+        }));
         return ok(views.html.players.render(players));
     }
 
--- app/models/Player.java
+++ app/models/Player.java
@@ -74,4 +74,44 @@ public class Player extends BaseModel<Player> {
         return !this.equals(guest);
     }
 
+    public int oneCreditCount() {
+        int oneCreditCount = 0;
+        for (Score score : bestScores()) {
+            if (score.stage != null && score.stage.name.toLowerCase().endsWith("all")) {
+                oneCreditCount++;
+            }
+        }
+        return oneCreditCount;
+    }
+
+    public int firstRankCount() {
+        int firstRankCount = 0;
+        for (Score score : bestScores()) {
+            if (score.rank() == 1) {
+                firstRankCount++;
+            }
+        }
+        return firstRankCount;
+    }
+
+    public int top3Count() {
+        int top3Count = 0;
+        for (Score score : bestScores()) {
+            if (score.rank() <= 3) {
+                top3Count++;
+            }
+        }
+        return top3Count;
+    }
+
+    public int top10Count() {
+        int top10Count = 0;
+        for (Score score : bestScores()) {
+            if (score.rank() <= 10) {
+                top10Count++;
+            }
+        }
+        return top10Count;
+    }
+
 }
