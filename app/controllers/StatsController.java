ba5b18725e70a02caf9ef2b9d13b33c5f11eabf0
Julien Smadja <julien.smadja@gmail.com>
2013-12-21 12:49:23 +0100

    Stats: Scores par total au fil du temps

--- app/controllers/StatsController.java
+++ app/controllers/StatsController.java
@@ -1,16 +1,35 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
+import com.google.common.base.Joiner;
 import models.Game;
 import models.Platform;
 import models.Score;
+import org.joda.time.DateMidnight;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.stats;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.avaje.ebean.Expr.le;
 
 public class StatsController extends Controller {
 
     public static Result index() {
         return ok(stats.render(Game.finder.all(), Platform.finder.all(), Score.finder.all()));
+    }
+
+    public static String scoresPerDay() {
+        List<String> scores = new ArrayList<String>();
+
+        DateMidnight dt = new DateMidnight(2013, 11, 29);
+        while (dt.isBeforeNow()) {
+            scores.add(Ebean.createQuery(Score.class).where(le("createdAt", dt.toDate())).findRowCount() + "");
+            dt = dt.plusDays(1);
+        }
+        return Joiner.on(",").join(scores);
     }
 
 }
