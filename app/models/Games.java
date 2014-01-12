6806c8df17b2391ccdefd0aa24ac79ec6cb528fe
Julien Smadja <julien.smadja@gmail.com>
2014-01-12 19:20:26 +0100

    Warning Play

--- app/models/Games.java
+++ app/models/Games.java
@@ -1,14 +1,17 @@
 package models;
 
-import javax.xml.bind.annotation.*;
-import java.util.Collection;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
 
 @XmlRootElement(name = "games")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Games {
 
-    @XmlElement(name= "game")
-    public Collection<Game> games;
+    @XmlElement(name = "game")
+    public List<Game> games;
 
     public Games() {
 
