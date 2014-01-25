8bc37fd1ec0f241cc0c96cc2c6d934a71178b718
Julien Smadja <julien.smadja@gmail.com>
2014-01-25 12:43:12 +0100

    Pages plateformes

---
+++ app/controllers/PlatformController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import models.Game;
+import models.Platform;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.platform_read;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlatformController extends Controller {
+
+    public static Result read(final String platformName) {
+        if (platformName == null) {
+            return notFound();
+        }
+        List<Game> games = Game.findAll();
+        games = new ArrayList<Game>(Collections2.filter(games, new Predicate<Game>() {
+            @Override
+            public boolean apply(@Nullable Game game) {
+                List<Platform> platforms = game.platforms;
+                for (Platform platform : platforms) {
+                    if (platform.name.equalsIgnoreCase(platformName)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }));
+
+        return ok(platform_read.render(platformName, games));
+    }
+
+}
--- app/plugins/ShmupClient.java
+++ app/plugins/ShmupClient.java
@@ -1,10 +1,11 @@
 package plugins;
 
+import com.gargoylesoftware.htmlunit.CookieManager;
+import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
-import com.google.common.io.Files;
+import com.gargoylesoftware.htmlunit.util.Cookie;
 
-import java.io.File;
 import java.io.IOException;
 
 public class ShmupClient {
@@ -44,6 +45,37 @@ public class ShmupClient {
         HtmlSubmitInput submitButton = loginForm.getInputByName("login");
         HtmlPage resultPage = submitButton.click();
         return resultPage.asText();
+    }
+
+    public static void main(String[] args) throws IOException {
+        // curl 'http://forum.shmup.com/posting.php?mode=reply&f=3&sid=0792e04c14a1aec2e59330b1a5b49a3c&t=18680' -H 'Cookie: phpbb3_axtcz_u=33489; phpbb3_axtcz_k=23c0adce886a4d0e; phpbb3_axtcz_sid=0792e04c14a1aec2e59330b1a5b49a3c' -H 'Origin: http://forum.shmup.com' -H 'Accept-Encoding: gzip,deflate,sdch' -H 'Accept-Language: fr-FR,fr;q=0.8,en;q=0.6' -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: max-age=0' -H 'Referer: http://forum.shmup.com/posting.php?mode=reply&f=3&t=18680' -H 'Connection: keep-alive' --data 'subject=Re%3A+%5BProjet%5D+Mise+a+jour+des+scores+automatique&addbbcode20=100&message=%5Bceci+est+un+test%5D&attach_sig=on&notify=on&topic_cur_post_id=437174&lastclick=1390595889&post=Envoyer&creation_time=1390595889&form_token=238a258f4f3a813a9e2b1169e4163ea7c5ee9170' --compressed
+
+        /*
+         POST /posting.php?mode=reply&f=3&sid=0792e04c14a1aec2e59330b1a5b49a3c&t=18680 HTTP/1.1
+        Host: forum.shmup.com
+        Connection: keep-alive
+        Content-Length: 268
+        Cache-Control: max-age=0
+        Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,q=0.8
+Origin: http://forum.shmup.com
+User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25
+Content-Type: application/x-www-form-urlencoded
+Referer: http://forum.shmup.com/posting.php?mode=reply&f=3&t=18680
+Accept-Encoding: gzip,deflate,sdch
+Accept-Language: fr-FR,fr;q=0.8,en;q=0.6
+Cookie: phpbb3_axtcz_u=33489; phpbb3_axtcz_k=23c0adce886a4d0e; phpbb3_axtcz_sid=0792e04c14a1aec2e59330b1a5b49a3c
+*/
+
+        ShmupClient shmupClient = new ShmupClient();
+        CookieManager cookieManager = shmupClient.webClient.getCookieManager();
+        cookieManager.addCookie(new Cookie("forum.shmup.com", "Origin", "http://forum.shmup.com"));
+        cookieManager.addCookie(new Cookie("forum.shmup.com", "phpbb3_axtcz_u", "33489"));
+        cookieManager.addCookie(new Cookie("forum.shmup.com", "phpbb3_axtcz_k", "23c0adce886a4d0e"));
+        cookieManager.addCookie(new Cookie("forum.shmup.com", "phpbb3_axtcz_sid", "0792e04c14a1aec2e59330b1a5b49a3c"));
+        HtmlPage page = shmupClient.webClient.getPage("http://forum.shmup.com/viewtopic.php?f=3&t=18680");
+        String textContent = page.getTextContent();
+        System.err.println(textContent);
+        System.err.println(page.getBody().getTextContent());
     }
 
 }
