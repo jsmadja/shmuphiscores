97e87a74683a03b024ae794dfab9f717b76fe386
jsmadja <julien.smadja@gmail.com>
2015-08-08 18:03:50 +0200

    Change medal colors

--- app/drawer/MedalsPicture.java
+++ app/drawer/MedalsPicture.java
@@ -11,6 +11,7 @@ import static com.avaje.ebean.Expr.and;
 import static com.avaje.ebean.Expr.eq;
 import static drawer.RankingGameConfiguration.COLOR_SHMUP_GREY;
 import static drawer.RankingGameConfiguration.COLOR_SHMUP_TITLE;
+import static java.awt.Font.PLAIN;
 import static java.awt.RenderingHints.KEY_ANTIALIASING;
 import static java.awt.RenderingHints.KEY_TEXT_ANTIALIASING;
 import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
@@ -21,25 +22,25 @@ import static org.apache.commons.lang3.StringUtils.leftPad;
 public class MedalsPicture {
 
     public static final int WIDTH = 150;
-    private final static Font gameFont = new Font("Liberation Mono", Font.PLAIN, 11);
+    private final static Font gameFont = new Font("Lucida", PLAIN, 11);
 
     public static BufferedImage createMedalsPicture(Player player) {
-        BufferedImage bi = new BufferedImage(WIDTH, 30, TYPE_INT_ARGB);
+        BufferedImage bi = new BufferedImage(WIDTH, 15, TYPE_INT_ARGB);
         Graphics2D graphics = bi.createGraphics();
         graphics.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
         graphics.setRenderingHint(KEY_TEXT_ANTIALIASING, VALUE_TEXT_ANTIALIAS_ON);
         FontMetrics fontMetrics = graphics.getFontMetrics();
 
         graphics.setColor(COLOR_SHMUP_GREY);
-        graphics.fillRect(0, 0, WIDTH, fontMetrics.getAscent() + 5);
+        graphics.fillRect(0, 0, WIDTH, 15);
         graphics.setFont(gameFont);
 
         Integer firstRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 1))).findRowCount();
-        draw(graphics, firstRankCount, 5, fontMetrics, Color.YELLOW);
+        draw(graphics, firstRankCount, 5, fontMetrics, new Color(255, 215, 0));
         int secondRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 2))).findRowCount();
-        draw(graphics, secondRankCount, 40, fontMetrics, Color.GRAY);
+        draw(graphics, secondRankCount, 40, fontMetrics, new Color(192, 192, 192));
         int thirdRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 3))).findRowCount();
-        draw(graphics, thirdRankCount, 75, fontMetrics, Color.ORANGE);
+        draw(graphics, thirdRankCount, 75, fontMetrics, new Color(205, 127, 50));
         int oneCreditCount = player.computeOneCredit();
         draw(graphics, oneCreditCount, 110, fontMetrics, Color.GREEN);
         return bi;
@@ -47,7 +48,7 @@ public class MedalsPicture {
 
     private static void draw(Graphics2D graphics, Integer firstRankCount, int i, FontMetrics fontMetrics, Color color) {
         graphics.setColor(color);
-        graphics.setFont(new Font("Liberation Mono", Font.PLAIN, 36));
+        graphics.setFont(new Font("Lucida", PLAIN, 36));
         graphics.drawString("• ", i, (float) fontMetrics.getAscent() * 1.5F);
         graphics.setFont(gameFont);
         graphics.setColor(COLOR_SHMUP_TITLE);
--- app/drawer/SignaturePicture.java
+++ app/drawer/SignaturePicture.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
 
 import static drawer.RankingGameConfiguration.COLOR_SHMUP_GREY;
 import static drawer.RankingGameConfiguration.COLOR_SHMUP_TITLE;
+import static java.awt.Font.PLAIN;
 import static java.awt.RenderingHints.KEY_ANTIALIASING;
 import static java.awt.RenderingHints.KEY_TEXT_ANTIALIASING;
 import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
@@ -15,17 +16,18 @@ import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON;
 import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
 
 public class SignaturePicture {
-    private final static Font gameFont = new Font("Liberation Mono", Font.PLAIN, 11);
+    public static final int WIDTH = 724 - 150;
+    private final static Font gameFont = new Font("Lucida", PLAIN, 11);
 
     public static BufferedImage createSignaturePicture(Player player) {
-        BufferedImage bi = new BufferedImage(724, 30, TYPE_INT_ARGB);
+        BufferedImage bi = new BufferedImage(WIDTH, 15, TYPE_INT_ARGB);
         Graphics2D graphics = bi.createGraphics();
         graphics.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
         graphics.setRenderingHint(KEY_TEXT_ANTIALIASING, VALUE_TEXT_ANTIALIAS_ON);
         FontMetrics fontMetrics = graphics.getFontMetrics();
 
         graphics.setColor(COLOR_SHMUP_GREY);
-        graphics.fillRect(0, 0, 724, fontMetrics.getAscent() + 5);
+        graphics.fillRect(0, 0, WIDTH, 15);
         graphics.setColor(COLOR_SHMUP_TITLE);
         graphics.setFont(gameFont);
         Score lastScore = player.getLastScore();
@@ -34,6 +36,6 @@ public class SignaturePicture {
     }
 
     private static String message(Score lastScore) {
-        return "Mon dernier score a été réalisé " + lastScore.formattedDate() + " sur " + lastScore.game.title + " (" + lastScore.formattedValue() + "pts - " + lastScore.formattedRank() + ")";
+        return "Dernier score réalisé " + lastScore.formattedDate() + " sur " + lastScore.game.title + " (" + lastScore.formattedValue() + "pts - " + lastScore.formattedRank() + ")";
     }
 }
