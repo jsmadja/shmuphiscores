06f9ecb9b197145e9f937d1b47626fe1433a7a00
jsmadja <julien.smadja@gmail.com>
2015-08-10 11:43:38 +0200

    Style medailles

--- app/drawer/MedalsPicture.java
+++ app/drawer/MedalsPicture.java
@@ -23,36 +23,41 @@ public class MedalsPicture {
 
     public static final int WIDTH = 145;
     private final static Font gameFont = new Font("Lucida", PLAIN, 11);
+    public static final float HEIGHT = 30;
 
     public static BufferedImage createMedalsPicture(Player player) {
-        BufferedImage bi = new BufferedImage(WIDTH, 15, TYPE_INT_ARGB);
+        BufferedImage bi = new BufferedImage(WIDTH, (int)HEIGHT, TYPE_INT_ARGB);
         Graphics2D graphics = bi.createGraphics();
         graphics.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
         graphics.setRenderingHint(KEY_TEXT_ANTIALIASING, VALUE_TEXT_ANTIALIAS_ON);
         FontMetrics fontMetrics = graphics.getFontMetrics();
 
         graphics.setColor(COLOR_SHMUP_GREY);
-        graphics.fillRect(0, 0, WIDTH, 15);
+        graphics.fillRect(0, 0, WIDTH, (int)HEIGHT);
         graphics.setFont(gameFont);
 
         Integer firstRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 1))).findRowCount();
-        draw(graphics, firstRankCount, 0, fontMetrics, new Color(255, 215, 0));
+        String picto = "";
+        picto = "✭";
+        picto = "✪";
+        picto = "✯";
+        draw(graphics, picto, firstRankCount, 0, fontMetrics, new Color(255, 215, 0));
         int secondRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 2))).findRowCount();
-        draw(graphics, secondRankCount, 35, fontMetrics, new Color(192, 192, 192));
+        draw(graphics, picto, secondRankCount, 35, fontMetrics, new Color(192, 192, 192));
         int thirdRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 3))).findRowCount();
-        draw(graphics, thirdRankCount, 70, fontMetrics, new Color(205, 127, 50));
+        draw(graphics, picto, thirdRankCount, 70, fontMetrics, new Color(205, 127, 50));
         int oneCreditCount = player.computeOneCredit();
-        draw(graphics, oneCreditCount, 105, fontMetrics, Color.GREEN);
+        draw(graphics, picto, oneCreditCount, 105, fontMetrics, Color.BLACK);
         return bi;
     }
 
-    private static void draw(Graphics2D graphics, Integer firstRankCount, int i, FontMetrics fontMetrics, Color color) {
+    private static void draw(Graphics2D graphics, String picto, Integer count, int i, FontMetrics fontMetrics, Color color) {
         graphics.setColor(color);
-        graphics.setFont(new Font("Lucida", PLAIN, 36));
-        graphics.drawString("• ", i, (float) fontMetrics.getAscent() * 1.8F);
+        graphics.setFont(new Font("Lucida", PLAIN, 30));
+        graphics.drawString(picto + " ", i, HEIGHT - HEIGHT/4);
         graphics.setFont(gameFont);
-        graphics.setColor(COLOR_SHMUP_TEXT);
-        graphics.drawString(pad(firstRankCount), i + 20, fontMetrics.getAscent());
+        //graphics.setColor(COLOR_SHMUP_TEXT);
+        graphics.drawString(pad(count), i + 22, HEIGHT - HEIGHT/4);
     }
 
     private static String pad(Integer value) {
