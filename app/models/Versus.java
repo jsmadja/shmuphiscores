b7936f09cdb82e041e40254204453ffbc99f2d82
jsmadja <julien.smadja@gmail.com>
2015-08-13 01:23:37 +0200

    Objectif personnel

--- app/models/Versus.java
+++ app/models/Versus.java
@@ -1,7 +1,12 @@
 package models;
 
+import org.joda.time.DateTime;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.math.RoundingMode.HALF_UP;
 
 public class Versus {
 
@@ -25,6 +30,7 @@ public class Versus {
         public final Mode mode;
         public final Score score1;
         public final Score score2;
+
         public Comparison(Game game, Difficulty difficulty, Mode mode, Score score1, Score score2) {
             this.game = game;
             this.difficulty = difficulty;
@@ -32,12 +38,26 @@ public class Versus {
             this.score1 = score1;
             this.score2 = score2;
         }
+
         public int diff() {
-            return this.score1.rank() > this.score2.rank() ? -1 :  1;
+            return this.score1.rank() > this.score2.rank() ? -1 : 1;
         }
 
-        public int gap() {
-            return -this.score1.rank() + this.score2.rank();
+        public int scoreGap() {
+            Score scoreA, scoreB;
+            if (score1.isTimeScore()) {
+                scoreA = score2;
+                scoreB = score1;
+                DateTime dateTimeA = new DateTime(scoreA.value.longValue());
+                DateTime dateTimeB = new DateTime(scoreB.value.longValue());
+                double a = dateTimeA.getMinuteOfDay() * 60000 + dateTimeA.getSecondOfMinute() * 1000 + dateTimeA.getMillisOfSecond();
+                double b = dateTimeB.getMinuteOfDay() * 60000 + dateTimeB.getSecondOfMinute() * 1000 + dateTimeB.getMillisOfSecond();
+                return (int)((a * 100D) / b);
+            } else {
+                scoreA = score1;
+                scoreB = score2;
+            }
+            return scoreA.value.multiply(BigDecimal.valueOf(100)).divide(scoreB.value, HALF_UP).intValue();
         }
     }
 }
