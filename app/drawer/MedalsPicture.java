655dc35288c4ce45b529baa61fb76ce658b7af99
jsmadja <julien.smadja@gmail.com>
2015-08-14 12:58:31 +0200

    Medailles

--- app/drawer/MedalsPicture.java
+++ app/drawer/MedalsPicture.java
@@ -3,18 +3,17 @@ package drawer;
 import models.Player;
 import models.Score;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import static com.avaje.ebean.Ebean.find;
 import static com.avaje.ebean.Expr.and;
 import static com.avaje.ebean.Expr.eq;
-import static drawer.RankingGameConfiguration.COLOR_SHMUP_GREY;
+import static drawer.RankingGameConfiguration.COLOR_SHMUP_TEXT;
 import static java.awt.Font.PLAIN;
-import static java.awt.RenderingHints.KEY_ANTIALIASING;
-import static java.awt.RenderingHints.KEY_TEXT_ANTIALIASING;
-import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
-import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON;
 import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
 import static org.apache.commons.lang3.StringUtils.leftPad;
 
@@ -25,33 +24,29 @@ public class MedalsPicture {
     private final static Font gameFont = new Font("Lucida", PLAIN, 11);
 
     public static BufferedImage createMedalsPicture(Player player, Color color) {
-        BufferedImage bi = new BufferedImage(WIDTH, (int) HEIGHT, TYPE_INT_ARGB);
-        Graphics2D graphics = bi.createGraphics();
-        graphics.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
-        graphics.setRenderingHint(KEY_TEXT_ANTIALIASING, VALUE_TEXT_ANTIALIAS_ON);
-        FontMetrics fontMetrics = graphics.getFontMetrics();
-
-        graphics.setColor(color);
-        graphics.fillRect(0, 0, WIDTH, (int) HEIGHT);
-        graphics.setFont(gameFont);
-
-        Integer firstRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 1))).findRowCount();
-        draw(graphics, "✯", firstRankCount, 0, fontMetrics, new Color(255, 215, 0));
-        int secondRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 2))).findRowCount();
-        draw(graphics, "✯", secondRankCount, 35, fontMetrics, new Color(192, 192, 192));
-        int thirdRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 3))).findRowCount();
-        draw(graphics, "✯", thirdRankCount, 70, fontMetrics, new Color(205, 127, 50));
-        int oneCreditCount = player.computeOneCredit();
-        draw(graphics, "✯", oneCreditCount, 105, fontMetrics, Color.BLACK);
+        BufferedImage bi = null;
+        try {
+            bi = ImageIO.read(new File("/Users/jsmadja/Desktop/poubelle/medailles.png"));
+            Graphics2D graphics = bi.createGraphics();
+            int space = 3;
+            Integer firstRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 1))).findRowCount();
+            draw(graphics, firstRankCount, 25 + space, COLOR_SHMUP_TEXT);
+            int secondRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 2))).findRowCount();
+            draw(graphics, secondRankCount, 60 + space, COLOR_SHMUP_TEXT);
+            int thirdRankCount = find(Score.class).where(and(eq("player", player), eq("rank", 3))).findRowCount();
+            draw(graphics, thirdRankCount, 95 + space, COLOR_SHMUP_TEXT);
+            int oneCreditCount = player.computeOneCredit();
+            draw(graphics, oneCreditCount, 140 + space, COLOR_SHMUP_TEXT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return bi;
     }
 
-    private static void draw(Graphics2D graphics, String picto, Integer count, int i, FontMetrics fontMetrics, Color color) {
+    private static void draw(Graphics2D graphics, Integer count, int i, Color color) {
         graphics.setColor(color);
-        graphics.setFont(new Font("Lucida", PLAIN, 30));
-        graphics.drawString(picto + " ", i, HEIGHT - HEIGHT / 4);
         graphics.setFont(gameFont);
-        graphics.drawString(pad(count), i + 22, HEIGHT - HEIGHT / 4);
+        graphics.drawString(pad(count), i, HEIGHT - HEIGHT / 4);
     }
 
     private static String pad(Integer value) {
