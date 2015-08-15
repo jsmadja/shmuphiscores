3ef5bd469eb7ec676bc7e2ff4c570859865f3a88
jsmadja <julien.smadja@gmail.com>
2015-08-15 22:47:50 +0200

    Formulaire de modification des jeux

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -1,11 +1,19 @@
 package controllers;
 
-import models.*;
+import actions.User;
+import models.Difficulty;
+import models.Game;
+import models.Mode;
+import models.Platform;
+import models.Ship;
+import models.Stage;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.game_read;
+import views.html.game_update;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class GameController extends Controller {
@@ -19,6 +27,16 @@ public class GameController extends Controller {
             return notFound();
         }
         return ok(game_read.render(game));
+    }
+
+    public static Result update(models.Game game) {
+        if (game == null) {
+            return notFound();
+        }
+        if (!User.current().isAuthenticated() && !User.current().isAdministrator()) {
+            return unauthorized();
+        }
+        return ok(game_update.render(game));
     }
 
     public static Result create() {
@@ -37,62 +55,105 @@ public class GameController extends Controller {
         return index(game);
     }
 
+    public static Result addPlatform(Game game) {
+        String platformName = request().body().asFormUrlEncoded().get("platform")[0];
+        createPlatform(game, platformName);
+        return update(game);
+    }
+
+    public static Result addDifficulty(Game game) {
+        Map<String, String[]> data = request().body().asFormUrlEncoded();
+        String difficultyName = data.get("difficulty")[0];
+        int sortOrder = Integer.parseInt(data.get("sortOrder")[0]);
+        createDifficulty(game, sortOrder, difficultyName);
+        return update(game);
+    }
+
+    public static Result addMode(Game game) {
+        Map<String, String[]> data = request().body().asFormUrlEncoded();
+        String modeName = data.get("mode")[0];
+        int sortOrder = Integer.parseInt(data.get("sortOrder")[0]);
+        createMode(game, sortOrder, modeName);
+        return update(game);
+    }
+
+    public static Result addShip(Game game) {
+        Map<String, String[]> data = request().body().asFormUrlEncoded();
+        String shipName = data.get("ship")[0];
+        int sortOrder = Integer.parseInt(data.get("sortOrder")[0]);
+        createShip(game, sortOrder, shipName);
+        return update(game);
+    }
+
+    private static void createPlatform(Game game, String platformName) {
+        Platform platform = new Platform(platformName);
+        platform.game = game;
+        platform.save();
+    }
+
     private static void createPlatforms(models.Game game) {
         Scanner sc = new Scanner(game.platforms.get(0).name);
-        if(sc.hasNextLine()) {
+        if (sc.hasNextLine()) {
             while (sc.hasNextLine()) {
-                Platform platform = new Platform(sc.nextLine().trim());
-                platform.game = game;
-                platform.save();
+                createPlatform(game, sc.nextLine().trim());
             }
         } else {
-            Platform platform = new Platform(" ");
-            platform.game = game;
-            platform.save();
+            createPlatform(game, " ");
         }
-
     }
 
     private static void createDifficulties(models.Game game) {
         Integer index = 0;
         Scanner sc = new Scanner(game.difficulties.get(0).name);
         while (sc.hasNextLine()) {
-            Difficulty difficulty = new Difficulty(sc.nextLine().trim());
-            difficulty.game = game;
-            difficulty.sortOrder = index.toString();
-            difficulty.save();
+            createDifficulty(game, index, sc.nextLine().trim());
             index++;
         }
+    }
+
+    private static void createDifficulty(Game game, Integer index, String difficultyName) {
+        Difficulty difficulty = new Difficulty(difficultyName);
+        difficulty.game = game;
+        difficulty.sortOrder = index.toString();
+        difficulty.save();
     }
 
     private static void createModes(models.Game game) {
         Integer index = 0;
         Scanner sc = new Scanner(game.modes.get(0).name);
         while (sc.hasNextLine()) {
-            Mode mode = new Mode(sc.nextLine().trim());
-            mode.game = game;
-            mode.sortOrder = index.toString();
-            mode.save();
+            createMode(game, index, sc.nextLine().trim());
             index++;
         }
+    }
+
+    private static void createMode(Game game, Integer index, String modeName) {
+        Mode mode = new Mode(modeName);
+        mode.game = game;
+        mode.sortOrder = index.toString();
+        mode.save();
     }
 
     private static void createShips(models.Game game) {
         Integer index = 0;
         Scanner sc = new Scanner(game.ships.get(0).name);
         while (sc.hasNextLine()) {
-            Ship ship = new Ship(sc.nextLine().trim());
-            ship.game = game;
-            ship.sortOrder = index.toString();
-            ship.save();
+            createShip(game, index, sc.nextLine().trim());
             index++;
         }
+    }
+
+    private static void createShip(Game game, Integer index, String shipName) {
+        Ship ship = new Ship(shipName);
+        ship.game = game;
+        ship.sortOrder = index.toString();
+        ship.save();
     }
 
     private static void createStages(models.Game game) {
         Long index = 0L;
         Scanner sc = new Scanner(game.stages.get(0).name);
-        if(sc.hasNextLine()) {
+        if (sc.hasNextLine()) {
             while (sc.hasNextLine()) {
                 Stage stage = new Stage(sc.nextLine().trim());
                 stage.game = game;
