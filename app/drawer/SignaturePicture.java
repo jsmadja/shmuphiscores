81009aaedd992311fe5a93babf417795ea8410bb
jsmadja <julien.smadja@gmail.com>
2015-09-18 18:14:31 +0200

    Taille "dernier score" qui n'est pas à 800px

--- app/drawer/SignaturePicture.java
+++ app/drawer/SignaturePicture.java
@@ -16,18 +16,13 @@ import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON;
 import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
 
 public class SignaturePicture {
-    public static final int WIDTH = 724 - 155;
+    public static final int WIDTH = 724;
     private final static Font gameFont = new Font("Lucida", PLAIN, 11);
 
     public static BufferedImage createSignaturePicture(Player player) {
         BufferedImage bi = new BufferedImage(WIDTH, 15, TYPE_INT_ARGB);
         Graphics2D graphics = bi.createGraphics();
-        //graphics.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
-        //graphics.setRenderingHint(KEY_TEXT_ANTIALIASING, VALUE_TEXT_ANTIALIAS_ON);
         FontMetrics fontMetrics = graphics.getFontMetrics();
-
-        //graphics.setColor(COLOR_SHMUP_GREY);
-        //graphics.fillRect(0, 0, WIDTH, 15);
         graphics.setColor(COLOR_SHMUP_TEXT);
         graphics.setFont(gameFont);
         Score lastScore = player.getLastScore();
